d428b7c31839bbd8b8126146a89daa2d1fecfdb7
Max Willian <max.silva@doalti.com.br>
2025-10-30 16:40:27 -0300

    Implantação de gateway para controlar requisições externas aos micro-serviços, implantação de portas aleatórias nos micro-serviços, e utilização de Hystrix para controlar falhas. OA#

--- hrpayroll/src/main/java/com/max/hrPayroll/resources/PaymentsResources.java
+++ hrpayroll/src/main/java/com/max/hrPayroll/resources/PaymentsResources.java
@@ -3,6 +3,7 @@ package com.max.hrPayroll.resources;
 
 import com.max.hrPayroll.entities.Payments;
 import com.max.hrPayroll.services.PaymentServices;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentsResources {
     @Autowired
     private PaymentServices paymentServices;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{idWorker}/days/{days}")
     public ResponseEntity<Payments> findById(@PathVariable Long idWorker, @PathVariable int days){
         Payments payment = paymentServices.getPayment(idWorker, days);
+        return ResponseEntity.ok().body(payment);
+    }
+
+    public ResponseEntity<Payments> getPaymentAlternative(Long idWorker, int days){
+        Payments payment = new Payments("Alternativo", 400.0 , days);
         return ResponseEntity.ok().body(payment);
     }
 }
