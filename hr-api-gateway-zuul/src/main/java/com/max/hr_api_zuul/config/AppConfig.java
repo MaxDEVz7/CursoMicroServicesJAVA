588c97a0bfbe8f70fa73caa9b7c82ce31cd773ed
Max Willian <max.silva@doalti.com.br>
2026-01-28 16:39:49 -0300

    Autorização e leitura do token pelo Gateway.

---
+++ hr-api-gateway-zuul/src/main/java/com/max/hr_api_zuul/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.max.hr_api_zuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("MAX TSTE");
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore jwtTokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+}
